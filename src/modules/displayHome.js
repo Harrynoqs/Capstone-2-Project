474dc54452f2616ee5c50b037e4b3b8e8b716814
Micheal <oguntayomicheal@gmail.com>
2022-10-29 14:30:53 +0100

    Fix linters errors

--- src/modules/displayHome.js
+++ src/modules/displayHome.js
@@ -1,68 +1,67 @@
 /* eslint-disable */
-import { homeItmes } from "./homeItemsCounter.js";
-import { getFromLocalStorage } from "./localStorage.js";
-import { commentDisplay } from "./commentDisplay.js";
-import { commentCounter } from "./commentFunctions.js";
+import { homeItmes } from './homeItemsCounter.js';
+import { getFromLocalStorage } from './localStorage.js';
+import { commentDisplay } from './commentDisplay.js';
+import { commentCounter } from './commentFunctions.js';
 
 export const displayList = () => {
-    const dataArray = getFromLocalStorage();
+  const dataArray = getFromLocalStorage();
   const display = document.getElementById('list');
 
-  const navBar = document.getElementById('products')
+  const navBar = document.getElementById('products');
 
-  navBar.innerHTML = ''
-  const span = document.createElement('span')
-  span.setAttribute('id', 'itemNum')
+  navBar.innerHTML = '';
+  const span = document.createElement('span');
+  span.setAttribute('id', 'itemNum');
 
+  navBar.appendChild(span);
 
-  navBar.appendChild(span)
+  display.innerHTML = '';
+  dataArray.forEach((each) => {
+    const eachCont = document.createElement('div');
+    eachCont.classList.add('each-container');
+    display.appendChild(eachCont);
 
-display.innerHTML = ''
-dataArray.forEach((each) => {
-    
+    const imgCont = document.createElement('div');
+    imgCont.classList.add('img-container');
+    imgCont.innerHTML = `<img src="${each.image}" alt="${each.category}">`;
+    eachCont.appendChild(imgCont);
 
-const eachCont = document.createElement('div')
-eachCont.classList.add('each-container')
-display.appendChild(eachCont);
+    const paraTitle = document.createElement('p');
+    paraTitle.classList.add('mens-cloth');
+    paraTitle.innerText = `${each.title}`;
+    eachCont.appendChild(paraTitle);
 
-const imgCont = document.createElement('div')
-imgCont.classList.add('img-container')
-imgCont.innerHTML = `<img src="${each.image}" alt="${each.category}">`
-eachCont.appendChild(imgCont)
+    const likeIcon = document.createElement('i');
+    likeIcon.setAttribute('class', 'fa-regular fa-heart');
+    likeIcon.setAttribute('data-id', `${each.id}`);
+    eachCont.appendChild(likeIcon);
 
-const paraTitle = document.createElement('p')
-paraTitle.classList.add('mens-cloth')
-paraTitle.innerText = `${each.title}`
-eachCont.appendChild(paraTitle)
+    const para1 = document.createElement('p');
+    para1.setAttribute('class', 'like-count');
+    para1.setAttribute('id', `${each.id}`);
+    para1.innerText = '0';
+    eachCont.appendChild(para1);
 
-const likeIcon = document.createElement('i')
-likeIcon.setAttribute('class', 'fa-regular fa-heart')
-likeIcon.setAttribute('data-id', `${each.id}`)
-eachCont.appendChild(likeIcon)
+    const para2 = document.createElement('p');
+    para2.setAttribute('class', 'like-text');
+    para2.innerText = 'Likes';
+    eachCont.appendChild(para2);
 
-const para1 = document.createElement('p')
-para1.setAttribute('class', 'like-count')
-para1.setAttribute('id', `${each.id}`)
-para1.innerText = '0'
-eachCont.appendChild(para1)
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('commentBtn');
+    deleteButton.innerText = 'comment';
+    deleteButton.setAttribute('id', `btn${each.id}`);
 
-const para2 = document.createElement('p')
-para2.setAttribute('class', 'like-text')
-para2.innerText = 'Likes'
-eachCont.appendChild(para2)
+    deleteButton.onclick = () => {
+      commentDisplay(`${each.id}`);
+      commentCounter();
+    };
+    eachCont.appendChild(deleteButton);
+  });
 
-const deleteButton = document.createElement('button')
-            deleteButton.classList.add('commentBtn')
-            deleteButton.innerText = 'comment'
-            deleteButton.setAttribute('id', `btn${each.id}`)
-            
-            deleteButton.onclick = () => {
-            commentDisplay(`${each.id}`)
-            commentCounter()
-            }
-            eachCont.appendChild(deleteButton)
-})
-
-  const itemNum = homeItmes()
+  const itemNum = homeItmes();
+  span.textContent = `Products (${itemNum})`;
+emNum = homeItmes()
   span.textContent = `Products (${itemNum})`
 };
--- src/modules/homeItemsCounter.js
+++ src/modules/homeItemsCounter.js
@@ -3,4 +3,4 @@ export const homeItmes = () => {
     const items = document.querySelectorAll('.each-container')
     const itemsArr = Array.from(items)
     return itemsArr.length
- }
+}
